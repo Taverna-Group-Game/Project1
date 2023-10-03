fddaf2685cec8c4537f7548e2c2164ff1bb6ce8e
Samuel Mariano Melo <89424721+Sam1536@users.noreply.github.com>
2023-10-02 23:37:40 -0300

    feat player movement

--- Assets/Runtime/Scripts/Player.cs
+++ Assets/Runtime/Scripts/Player.cs
@@ -3,26 +3,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Vector2 DirectionInput;
-    public float Speed;
+    private Movement movement;
     private Rigidbody2D rig;
 
-
-    private void Awake()
-    {
-        
-    }
+    public AtributtsMovement atributtsMovement;
 
     private void Start()
     {
+        movement = new();
         rig = GetComponent<Rigidbody2D>();
+        movement.Atributts = atributtsMovement;
     }
-
 
     private void Update()
     {
+        int plyer = (int)Input.GetAxis("Horizontal");
         
-    }
+        movement.MoveObject(plyer, rig);
 
+    }
 
 }
